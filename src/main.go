08950753700c461f7f17ba3b3b97680c15779064
Turboman3000 <turboman@turboman.de>
2025-01-06 15:26:13 +0100

    feat: Added comments to entire code

--- src/main.go
+++ src/main.go
@@ -21,42 +21,50 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Define a flag for the Minecraft version
 var versionFlag = flag.String("version", "1.21.4", "Minecraft Version")
 
 func main() {
+	// Load environment variables from .env file
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
 
+	// Parse command-line flags
 	flag.Parse()
 
+	// Create a temporary directory for the downloaded files
 	var tempDir = filepath.Join(os.TempDir(), "/CEMCAU_"+*versionFlag)
+	// Construct the download URL for the Minecraft assets
 	var dlURL = "https://github.com/InventivetalentDev/minecraft-assets/zipball/refs/heads/" + *versionFlag
 
 	fmt.Println("Dowloading from " + dlURL)
 
+	// Create a file to save the downloaded zip
 	out, err := os.Create(tempDir + ".zip")
 
 	if err != nil {
 		panic(err)
 	}
-
 	defer out.Close()
 
+	// Download the zip file
 	resp, err := http.Get(dlURL)
 
 	if err != nil {
 		panic(err)
 	}
-
 	defer resp.Body.Close()
 
+	// Copy the downloaded data to the file
 	io.Copy(out, resp.Body)
 
+	// Unpack the downloaded zip file
 	unpackZip(tempDir)
 }
 
+// Unpacks the zip file to the specified directory
 func unpackZip(tempDir string) {
 	zipReader, err := zip.OpenReader(tempDir + ".zip")
 
@@ -72,6 +80,7 @@ func unpackZip(tempDir string) {
 
 		var rootIndex = 0
 
+		// Find the root directory of the extracted files
 		for i, part := range fileParts {
 			if strings.HasPrefix(part, "InventivetalentDev-minecraft-assets") {
 				filePath = strings.ReplaceAll(filePath, part, "")
@@ -80,6 +89,7 @@ func unpackZip(tempDir string) {
 			}
 		}
 
+		// Skip certain directories and files
 		if len(fileParts) >= 9 && fileParts[rootIndex+1] == "data" {
 			os.Remove(tempDir + ".zip")
 			generateObjects(tempDir)
@@ -107,11 +117,13 @@ func unpackZip(tempDir string) {
 			continue
 		}
 
+		// Validate the file path
 		if !strings.HasPrefix(filePath, filepath.Clean(tempDir)+string(os.PathSeparator)) {
 			fmt.Println("invalid file path")
 			return
 		}
 
+		// Create directories if necessary
 		if file.FileInfo().IsDir() {
 			os.MkdirAll(filePath, os.ModePerm)
 			continue
@@ -123,6 +135,7 @@ func unpackZip(tempDir string) {
 
 		fmt.Println(file.Name)
 
+		// Extract the file
 		dstFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
 		if err != nil {
 			panic(err)
@@ -142,6 +155,7 @@ func unpackZip(tempDir string) {
 	}
 }
 
+// Structure for the index file
 type IIndex struct {
 	Hash     string   `json:"hash"`
 	Path     string   `json:"path"`
@@ -149,10 +163,12 @@ type IIndex struct {
 	Versions []string `json:"versions"`
 }
 
+// Generates the index file and objects
 func generateObjects(tempDir string) {
 	var index []IIndex
 	var objectPath = tempDir + "_objects"
 
+	// Walk through the assets directory
 	filepath.Walk(filepath.Join(tempDir, "assets", "minecraft"), func(path string, info os.FileInfo, err error) error {
 		var fPath = strings.ReplaceAll(strings.ReplaceAll(path, tempDir, ""), "\\", "/")
 
@@ -167,6 +183,7 @@ func generateObjects(tempDir string) {
 			panic(err)
 		}
 
+		// Calculate the MD5 hash of the file
 		h := md5.New()
 		if _, err := io.Copy(h, file); err != nil {
 			panic(err)
@@ -174,16 +191,17 @@ func generateObjects(tempDir string) {
 
 		hash := fmt.Sprintf("%x", h.Sum(nil))
 
+		// Create the directory for the hash
 		var basePath = filepath.Join(objectPath, hash[0:2])
-
 		os.MkdirAll(basePath, os.ModePerm)
 
+		// Read the file content and write it to the new location
 		fileContent, _ := os.ReadFile(path)
-
 		os.WriteFile(filepath.Join(basePath, hash), fileContent, 0644)
 
 		fmt.Println(fileParts[len(fileParts)-1] + " -> " + hash[0:2] + "/" + hash)
 
+		// Add the file to the index
 		index = append(index, IIndex{
 			Hash:     hash,
 			Path:     fPath,
@@ -193,8 +211,8 @@ func generateObjects(tempDir string) {
 		return nil
 	})
 
+	// Marshal the index to JSON and write it to a file
 	jsonString, _ := json.Marshal(index)
-
 	indexFile, err := os.Create(filepath.Join(objectPath, "index.json"))
 
 	if err != nil {
@@ -204,10 +222,12 @@ func generateObjects(tempDir string) {
 
 	io.WriteString(indexFile, string(jsonString[:]))
 
+	// Remove the temporary directory
 	os.Remove(tempDir)
 	uploadToS3(objectPath)
 }
 
+// Uploads the generated objects to S3
 func uploadToS3(objectPath string) {
 	pathStyle, _ := strconv.ParseBool(os.Getenv("S3_PATH_STYLE"))
 
@@ -218,8 +238,10 @@ func uploadToS3(objectPath string) {
 	}, func(o *s3.Options) {
 		o.UsePathStyle = pathStyle
 	})
+
 	uploader := manager.NewUploader(client)
 
+	// Walk through the object directory and upload each file to S3
 	filepath.Walk(objectPath, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
@@ -252,5 +274,4 @@ func uploadToS3(objectPath string) {
 
 		return nil
 	})
-
 }
