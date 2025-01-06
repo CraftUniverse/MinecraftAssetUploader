63ea56f617c2bba7a7b208fd01fd417ba3dbc42e
Turboman3000 <turboman@turboman.de>
2025-01-06 16:14:38 +0100

    feat(build): add Windows build support for arm64

--- build.bat
+++ build.bat
@@ -15,3 +15,7 @@ go build -ldflags="-s -w" -o dist/linux/CEMCAU-linux-arm64 src/main.go
 SET GOOS=darwin
 
 go build -ldflags="-s -w" -o dist/darwin/CEMCAU-darwin-arm64 src/main.go
+
+SET GOOS=windows
+
+go build -ldflags="-s -w" -o dist/win/CEMCAU-win32-arm64.exe src/main.go
